79b5edad5bcee8023862c3e7c30904f8a3afb78e
AMchuAMchuAMchu <2466130840@qq.com>
2023-03-04 13:56:02 +0800

    基本数据类型的一些注意问题

---
+++ src/main/java/com/atguigu/struct/Struct1001.java
@@ -0,0 +1,32 @@
+package com.atguigu.struct;
+
+/**
+ * Description ==> TODO
+ * BelongsProject ==> _01_Java
+ * BelongsPackage ==> com.atguigu.struct
+ * Version ==> 1.0
+ * CreateTime ==> 2023-03-04 13:38:41
+ * Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
+ */
+public class Struct1001 {
+
+    public static void main(String [] args){
+        int i = 0;
+        long l001 = 100;
+//      当你用的是包装类型的话这个时候如果还是用的是字面量的形式赋值的话即会报错的说,解决:后面加上l/L,"l"的话还是会报警的说,"L"的话则不会报错
+        Long l01 = 100l;
+        Long l010 = 100L;
+//
+        float f01 = 100;
+        float f02 = 100.0f;
+        float f0201 = 100.0f;
+        Float f03 = 100f;
+        double d01 = 100.0;
+        Double d02 = 100D;
+        Double d03 = 100d;
+
+    }
+
+
+
+}
