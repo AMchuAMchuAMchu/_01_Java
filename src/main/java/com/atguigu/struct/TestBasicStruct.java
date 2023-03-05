00628da7c4e022c6fcbbd018d23b9ee2e42658fb
AMchuAMchuAMchu <2466130840@qq.com>
2023-03-05 18:12:42 +0800

    简单算法

--- src/main/java/com/atguigu/struct/TestBasicStruct.java
+++ src/main/java/com/atguigu/struct/TestBasicStruct.java
@@ -2,6 +2,10 @@ package com.atguigu.struct;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * Description ==> TODO
  * BelongsProject ==> _01_Java
@@ -15,6 +19,274 @@ public class TestBasicStruct {
     private static int i03;
 
     private final int I04 = 44;
+
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String next = scanner.next();
+        int i = scanner.nextInt();
+        System.out.println("next:" + next);
+        System.out.println("i:" + i);
+
+    }
+
+    //day07
+    @Test
+    public void testBubble() {
+
+        int[] arr = {12, 34, 7, 9, 47, 33, 89, 21, 56, 83};
+
+        System.out.println("排序之前:" + Arrays.toString(arr));
+
+        long start = System.currentTimeMillis();
+
+        for (int k = 0; k < 100000000; k++) {
+
+//            我们自己写的
+//            int circle = 0;
+//            for (int i = 0; i < arr.length - 1 - circle; i++) {
+//                if (arr[i] > arr[i + 1]) {
+//                    arr[i] = arr[i + 1] ^ arr[i];
+//                    arr[i + 1] = arr[i + 1] ^ arr[i];
+//                    arr[i] = arr[i + 1] ^ arr[i];
+//                }
+//                if (i == (arr.length - 2 - circle)) {
+//                    i = -1;
+//                    circle++;
+//                }
+//            }
+
+//          老师的写法3356
+            for(int i = 0;i < arr.length - 1;i++){
+                for(int j = 0;j < arr.length - 1 - i;j++){
+                    if(arr[j] > arr[j + 1]){
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+
+        }
+
+
+        long end = System.currentTimeMillis();
+
+
+        System.out.println("耗时:" + (end - start));//5722//5711
+
+        System.out.println("排序之后:" + Arrays.toString(arr));
+
+    }
+
+
+    @Test
+    public void testArr() {
+
+        String[] strArr = new String[5];
+        int[] intArr = new int[5];
+        boolean[] booArr = new boolean[5];
+
+        System.out.println(Arrays.toString(strArr));
+        System.out.println(Arrays.toString(intArr));
+        System.out.println(Arrays.toString(booArr));
+
+    }
+
+    @Test
+    public void testTraverse() {
+
+        int[][] arr = new int[][]{{1, 2, 3}, {4, 5}, {6, 7, 8}};
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.println(arr[i][j]);
+            }
+        }
+
+
+    }
+
+
+    @Test
+    public void testArrayInit() {
+
+        String[] animeArray01 = {};
+        String[] animeArray04 = new String[]{};
+        String[] animeArray02 = new String[10];
+        String[] animeArray03 = new String[]{"全职猎人", "来自深渊", "约定的梦幻岛"};
+
+        String[][] hehe = new String[10][10];
+        String[][] hehe01 = new String[][]{new String[1]};
+        String[][] heha = {};
+
+        System.out.println(Arrays.toString(animeArray01));
+        System.out.println(Arrays.toString(animeArray02));
+        System.out.println(Arrays.toString(animeArray03));
+
+    }
+
+
+    @Test
+    public void testDotted() {
+
+        for (int i = 6; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+    }
+
+    @Test
+    public void testWanShu() {
+        for (int i = 2; i <= 1000; i++) {
+            int sum = 0;
+            for (int j = 1; j < i; j++) {
+                if (i % j == 0) {
+                    sum += j;
+                }
+//                这里的话是记得到最后再开始算总数的说,不然的话会出现一些约束之和的却会超过i的情况...
+                if (j == (i - 1)) {
+                    if (sum == i) {
+                        System.out.println("完数:" + i);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+
+    @Test
+    public void testOdd() {
+        int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 != 0) {
+                System.out.println("奇数:" + i);
+                sum += i;
+            }
+        }
+        System.out.println("sum:" + sum);
+    }
+
+
+    @Test
+    public void testSwap() {
+        int num01 = 10;
+        int num02 = 20;
+//        int num03;
+//        1.0
+//        num03 = num01;
+//        num01 = num02;
+//        num02 = num03;
+//        2.0
+//        num01 = num01 ^ num02;
+//        num02 = num01 ^ num02;
+//        num01 = num01 ^ num02;
+//        3.0
+//        num01 = num01 +num02;
+//        num02 = num01 - num02;
+//        num01 = num01-num02;
+
+
+        System.out.println(num01);
+        System.out.println(num02);
+
+        Byte num = 10;
+        switch (num) {
+            case 1:
+                System.out.println("ooo" + num);
+                break;
+        }
+    }
+
+
+    @Test
+    public void testMaxInThree() {
+
+        int num01 = 170;
+        int num02 = 900;
+        int num03 = 8700;
+
+//        int max = num01 > num02?(num01 > num03?num01:num03):(num02 > num03?num02:num03);
+
+        if (num01 > num02) {
+            if (num01 > num03) {
+                System.out.println(num01);
+            } else {
+                System.out.println(num03);
+            }
+        } else {
+            if (num02 > num03) {
+                System.out.println(num02);
+            } else {
+                System.out.println(num03);
+            }
+        }
+
+//        System.out.println(max);
+
+
+    }
+
+
+    @Test
+    public void testAndAnd() {
+
+//        boolean b01 = true;
+//
+//        int i = 10;
+//
+//        if ((++i == 10) && (b01 = false)) {
+//        }
+//        System.out.println(b01);
+        boolean x = true;
+        boolean y = false;
+        short z = 40;
+        if ((z++ == 40) && (y = true)) {
+            z++;
+        }
+        if ((x = false) || (++z == 43)) {
+            z++;
+        }
+        System.out.println("z = " + z);
+
+    }
+
+
+    @Test
+    public void testBooleanAndFloat() {
+
+//        float f01 = 100.0f;
+//        System.out.println(Boolean.TRUE);
+//        System.out.println(Float.MAX_VALUE);
+
+        int i01 = 200;
+        byte b01 = (byte) i01;
+
+        System.out.println(b01);
+
+    }
+
+
+    @Test
+    public void testTernaryOperation() {
+
+
+        int 雪乃 = 11;
+
+        int u66u = 11;
+
+        int i = new Random().nextInt(10);
+        System.out.println(i);
+        System.out.println(i % 2 == 0 ? "雪乃" : "楪祈");
+
+    }
 
 
     @Test
